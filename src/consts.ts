03a627f1d15863a18cde20631a8279d6ec5eafb6
Your Name <you@example.com>
2026-03-18 21:32:57 +0600

    changes

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "OpenBolna";
+export const SITE_DESCRIPTION = "Open Source AI Voice Agents for Outbound and Inbound Calling.";
